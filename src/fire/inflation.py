179ed3ae7f2d6bf801e3e2a35ef9d982278992ba
Michal Wrobel <sparrovv@gmail.com>
2024-04-15 16:18:57 +0200

    Read inflation from file generator

--- src/fire/inflation.py
+++ src/fire/inflation.py
@@ -7,3 +7,37 @@ def random_inflation_gen(range: tuple[int, int]) -> Generator[Decimal, None, Non
     while True:
         random_between_range = randrange(range[0], range[1]) / 100
         yield Decimal(str(random_between_range))
+
+
+def inflation_from_file_gen(
+    inflation_path: str, monthly: bool = False
+) -> Generator[Decimal, None, None]:
+    """
+    the file is a CSV with the first column being the date and the second column being the inflation rate.
+
+    This function will load the file and yield the inflation rate,
+    once the file finish it will start again from the beginning.
+
+    If monthly arg is True, it will divide the inflation rate by 12 to get the monthly inflation rate and
+    return it 12 times.
+    """
+
+    with open(inflation_path, "r") as file:
+        data = file.readlines()
+        assert data[0].strip() == "date,value"
+        while True:
+            for line in data:
+                if line.strip() == "date,value":
+                    continue
+                date, inflation_rate = map(
+                    lambda x: x.strip(),
+                    line.split(","),
+                )
+
+                if monthly:
+                    yield Decimal(inflation_rate)
+                else:
+                    for _ in range(12):
+                        yield round(Decimal(inflation_rate) / Decimal("12"), 3)
+
+            file.seek(0)
--- src/fire/test_inflation.py
+++ src/fire/test_inflation.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
-from .inflation import random_inflation_gen
+from .inflation import random_inflation_gen, inflation_from_file_gen
+import tempfile
 
 
 def test_random_inflation_gen() -> None:
@@ -7,3 +8,29 @@ def test_random_inflation_gen() -> None:
     assert next(g) >= Decimal("0.01") and next(g) <= Decimal("0.05")
     assert next(g) >= Decimal("0.01")
     assert next(g) >= Decimal("0.01")
+
+
+def test_from_file_gen() -> None:
+    inflation_date = """date, value
+    2020, 0.024
+    2021, 0.08
+    2022, 0.14
+"""
+
+    file_path = None
+
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp:
+        temp.write(inflation_date)
+        file_path = temp.name
+
+    gen = inflation_from_file_gen(file_path)
+
+    for _ in range(12):
+        assert next(gen) == Decimal("0.002")
+
+    for _ in range(12):
+        assert next(gen) == Decimal("0.007")
+
+    for _ in range(30):
+        # that it doesn't fail
+        assert next(gen)
